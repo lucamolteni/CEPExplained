8148567fbc02c50e2d3b256dec8d2b9fbceb22a7
Luca Molteni <volothamp@gmail.com>
2021-10-13 12:25:16 +0200

    Test using pseudo clock

--- src/test/java/kie/live/RuleTest.java
+++ src/test/java/kie/live/RuleTest.java
@@ -11,32 +11,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package kie.live;
 
-import static org.junit.Assert.*;
-
 import java.time.Instant;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Deque;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.drools.core.time.SessionPseudoClock;
 import org.junit.Test;
 import org.kie.api.KieBase;
-import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
-import org.kie.api.builder.Message;
-import org.kie.api.builder.Results;
-import org.kie.api.conf.EventProcessingOption;
-import org.kie.api.definition.KiePackage;
-import org.kie.api.definition.rule.Rule;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.KieSessionConfiguration;
@@ -44,7 +32,10 @@ import org.kie.api.runtime.conf.ClockTypeOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertEquals;
+
 public class RuleTest {
+
     static final Logger LOG = LoggerFactory.getLogger(RuleTest.class);
 
     @Test
@@ -69,6 +60,34 @@ public class RuleTest {
 
         session.fireAllRules();
 
-        assertEquals(hb1Date, check.pop().getTs());
+        assertEquals(hb1, check.pop());
+    }
+
+    @Test
+    public void testUsingPseudoClock() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kContainer = kieServices.getKieClasspathContainer();
+        KieBase kieBase = kContainer.getKieBase("CEPExplained");
+
+        KieSessionConfiguration conf = KieServices.Factory.get().newKieSessionConfiguration();
+
+        conf.setOption(ClockTypeOption.PSEUDO);
+        KieSession session = kieBase.newKieSession(conf, null);
+
+        Deque<HeartBeat> check = new ArrayDeque<>();
+        session.setGlobal("controlSet", check);
+
+        SessionPseudoClock clock = session.getSessionClock();
+
+        HeartBeat hb1 = new HeartBeat();
+        Date hb1Date = Date.from(Instant.ofEpochSecond(clock.getCurrentTime()));
+        hb1.setTs(hb1Date);
+
+        session.insert(hb1);
+        clock.advanceTime(5, TimeUnit.SECONDS);
+
+        session.fireAllRules();
+
+        assertEquals(hb1, check.pop());
     }
 }
